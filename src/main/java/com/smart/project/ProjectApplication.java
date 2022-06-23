886819303460fd1d209b416d94bae217371c0b7f
smhrd <oocheol@naver.com>
2022-06-23 19:46:28 +0900

    22/06/23

--- src/main/java/com/smart/project/ProjectApplication.java
+++ src/main/java/com/smart/project/ProjectApplication.java
@@ -1,12 +1,10 @@
 package com.smart.project;
 
-import com.smart.project.proc.SqlTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
